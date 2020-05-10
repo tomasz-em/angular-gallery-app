93d22292c44416b08ea5c10194e0c2caf02ad677
Tomasz-eM <56788219+tomasz-em@users.noreply.github.com>
2020-05-10 11:23:39 +0200

    added basic interactive behavior
    * change interval by 3 seconds
    * click event enabled

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -5,9 +5,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent {
+/*   pictures: Array <string>;
+  currentIndex: Number; */
 
-  constructor() { }
+    pictures = [
+      'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      'https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      'https://images.unsplash.com/photo-1516259762381-22954d7d3ad2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1066&q=80'
+    ];
+    currentIndex = 0; // początkowa waertość
+
+  constructor() {
+    setInterval( () => {
+      if ( this.currentIndex < ( this.pictures.length - 1 ) ) this.currentIndex = this.currentIndex + 1;
+      else this.currentIndex = 0;
+    }, 3000 );
+   }
+
+   handleClick( index ) {
+     this.currentIndex = index;
+   }
 
   ngOnInit(): void {
   }
