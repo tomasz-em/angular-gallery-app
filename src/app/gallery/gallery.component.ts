87b3b680acd524b7d008fe0201be28a290d2d647
Tomasz-eM <56788219+tomasz-em@users.noreply.github.com>
2020-05-13 23:20:38 +0200

    pause slideshow on demand along with better UI/UX/a11y
    * redefined slide show logic
      - introduced functions for starting and stopping the slide show as well as relevant status variables
      - the show starts automatically, but can be stopped for any time by the user
    
    * added two events related to the mouse pointer to the logic
      - mouse "enters" and "leaving" outside the element
      - events relate to the preview of the current gallery item
    
    * added the ability to stop the slide show when the cursor is over the displayed image
      - the slideshow delay is RESET each time you change picture on demand
      - ... applies to NEXT/BACK navigation and any thumbnail selection
    
    * conditional display of content during pause
      - textual content in the pause element
      - CAUTION: replacing "ITEM FOR ITEM" DOESN'T START ANIMATIONS DEFINED IN CSS!
      - ... that's why added the conditional display of content in THE SAME ELEMENT based on the logic of an additional class that changes element appearance
      - added border and mouse cursor change when hovering over the displayed main image
    
    * UI/UX/a11y: Increased size of navigation buttons BACK/NEXT for easier navigating on touch screens
    
    * added animation effects to various elements when their appearance changes due to the internal pause state of the component

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -11,7 +11,8 @@ export class GalleryComponent {
   currentIndex: number = 0; // bieżąca pozycja przeglądana w powiększeniu; zaczynamy od ZERA, tj. od POCZĄTKU TABLICY
   timeInterval: number = 5000; // czas wyświetlenia bieżącego obrazka / przejścia pomiędzy slajdami
   rememberedInterval: any;  // wskaźnik na czas przejścia
-  isPaused: boolean = false;  // stan pauzy - czy przegląd slajdów jest wstrzymany?
+  isPaused: boolean = true;  // stan pauzy - czy przegląd slajdów jest wstrzymany?
+  wasInterrupted: boolean = false;  // czy jakkolwiek JUŻ zablokowano pauzę? 
 
   constructor() {
     // warto użyć tablicy obiektów, np. by pozyskać "alt" czyt jakieś inne treści
@@ -38,32 +39,58 @@ export class GalleryComponent {
       }
     ];
 
+    this.startSlideshow();
     //currentInterval = setInterval( ...) ... by zatrzymać pokaz po kliknieciu i wystartować czas od tego kliknięcia... l8b blokwoać gdy hover    
 
-    // startInterval() {...}
-    // stopInterval() {...}
+  } //   // constructor-END
+
+    slideshow() {
+      if ( this.currentIndex < ( this.pictures.length - 1 ) ) this.currentIndex = this.currentIndex + 1;  // efektywny zakres to MAX-2
+      else this.currentIndex = 0;
+      this.wasInterrupted = false;
+    }
+
+    startSlideshow() {
+        // ale nie wyzwalaj tego wielorotnie
+      //if ( !this.wasInterrupted ) {
+        this.stopSlideshow();
+        // this.rememberedInterval = setInterval( this.slideShow, this.timeInterval );  // wykonaj wielokrotnie zapamiętaną funkcję
+        this.rememberedInterval = setInterval( () => {
+            this.slideshow();  // jawne wywołanie jako podległośc tej funkcji
+
+          }, this.timeInterval );
+        this.isPaused = false;
+      //}  
+    }
+
+    stopSlideshow() {
+      // console.log('PRZED zatrzymaniem', this.rememberedInterval );
+      clearInterval( this.rememberedInterval );
+      // console.log('PO zatrzymaniu', this.rememberedInterval );
+      this.wasInterrupted = true; // aktywacja blokady na  PONOWNĄ aktywację zdarzenia, by uniknąć wieloktrotnych wywołań
+      this.isPaused = true;
+    }
     // timeInterval =
 
-    setInterval( () => {
-      if ( this.currentIndex < ( this.pictures.length - 1 ) ) this.currentIndex = this.currentIndex + 1;
-      else this.currentIndex = 0;
-    }, this.timeInterval );
 
-  } //   // constructor-END
+
 
    handleClick( passedIndex ) {
     if ( passedIndex < ( this.pictures.length ) && ( passedIndex >= 0 ) )
     this.currentIndex = passedIndex;
+    this.startSlideshow();
    }
 
    handlePrev () { // logikę  poprawić o przejście na początek/koniec
     if ( this.currentIndex === 0 ) this.currentIndex = this.pictures.length - 1;
     else this.currentIndex = this.currentIndex - 1;
+    this.startSlideshow();  // + restart czasu pokazywania DOCELOWEGO slajdu
    }
 
    handleNext () {
       if ( this.currentIndex === ( this.pictures.length - 1) ) this.currentIndex = 0;
       else this.currentIndex = this.currentIndex + 1;
+      this.startSlideshow();  // + restart czasu pokazywania DOCELOWEGO slajdu
    }
 
   /* ngOnInit(): void {  // i tak nie zadziała, nie jest teraz wykorzystywane
